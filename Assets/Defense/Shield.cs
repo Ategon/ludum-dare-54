58cf8b3d9004db5d465855e8c1813958ee9cb644
Ategon <benjamin@barbeau.net>
2023-09-29 20:34:53 -0400

    Shield movement

---
+++ Assets/Defense/Shield.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private InputHandler inputs;
+
+    private void Start()
+    {
+        inputs = FindObjectOfType<InputHandler>();
+    }
+
+    private void Update()
+    {
+        if (inputs.Input.x > 0)
+        {
+            transform.position = new Vector3(0.25f, 0, 0);
+        }
+        else if (inputs.Input.x < 0)
+        {
+            transform.position = new Vector3(-0.25f, 0, 0);
+        }
+        else if (inputs.Input.y > 0)
+        {
+            transform.position = new Vector3(0, 0.25f, 0);
+        }
+        else if (inputs.Input.y < 0)
+        {
+            transform.position = new Vector3(0, -0.25f, 0);
+        }
+    }
+}
