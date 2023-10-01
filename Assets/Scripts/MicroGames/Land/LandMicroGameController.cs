27e9838c762fc07bb0743c174b0eae3213633999
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-01 17:06:17 -0400

    Changed spaceship art.

--- Assets/Scripts/MicroGames/Land/LandMicroGameController.cs
+++ Assets/Scripts/MicroGames/Land/LandMicroGameController.cs
@@ -1,11 +1,13 @@
 namespace Auboreal {
 
 	public class LandMicroGameController : AMicroGameController {
-		
+
 		public LandSpaceship landSpaceship;
-		
+		public LandPlatformLocator landPlatformLocator;
+
 		protected override void OnGameStarted() {
 			base.OnGameStarted();
+			landPlatformLocator.OnGameStarted();
 			landSpaceship.OnGameStarted();
 		}
 
