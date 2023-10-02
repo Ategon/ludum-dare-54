e260880f36adbe288cf0c425ccee58fb4ac20973
NnYGames <NnYMakingGames@gmail.com>
2023-10-02 19:49:03 +0300

    Finished implementing fuelup

--- Assets/Scripts/MicroGames/FuelUp/FuelUpSpaceship.cs
+++ Assets/Scripts/MicroGames/FuelUp/FuelUpSpaceship.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class FuelUpSpaceship : MonoBehaviour
 {
-    [SerializeField] Transform spacestation;
+    [SerializeField] private Transform spacestation;
+    [SerializeField] private float speed = 10f;
+
+    private bool move = true;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 look = transform.InverseTransformPoint(spacestation.position);
+        /*Vector3 look = transform.InverseTransformPoint(spacestation.position);
         float angle = Mathf.Atan2(look.y, look.x) * Mathf.Rad2Deg;
 
-        transform.Rotate(0, 0, angle);
+        transform.Rotate(0, 0, angle);*/
         //transform.LookAt(spacestation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (move)
+            transform.position = Vector2.MoveTowards(transform.position, spacestation.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "FuelLocation")
+        {
+            Debug.Log("Success");
+            collision.transform.GetComponentInParent<SpaceStation>().rotate = false;
+            move = false;
+        }
+        if (collision.gameObject.name == "Spacestation")
+        {
+            Debug.Log("Fail");
+            collision.transform.GetComponent<SpaceStation>().rotate = false;
+            move = false;
+        }
     }
 }
--- Assets/Scripts/MicroGames/FuelUp/SpaceStation.cs
+++ Assets/Scripts/MicroGames/FuelUp/SpaceStation.cs
@@ -7,6 +7,9 @@ public class SpaceStation : MonoBehaviour
 {
     private InputHandler inputs;
     [SerializeField] private float rotationSpeed;
+
+    private float moveDir = 1;
+    public bool rotate = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,11 @@ public class SpaceStation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = new Quaternion(0, 0, transform.rotation.z - inputs.Input.x * rotationSpeed * Time.deltaTime, transform.rotation.w);
+        if (inputs.Input.x != 0)
+            moveDir = inputs.Input.x;
+        
+        if (rotate)
+            transform.rotation = new Quaternion(0, 0, transform.rotation.z - moveDir * rotationSpeed * Time.deltaTime, transform.rotation.w);
+        //transform.Rotate(0, 0, transform.rotation.z + moveDir * rotationSpeed * Time.deltaTime);
     }
 }
