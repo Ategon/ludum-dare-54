177c70f9f9972e58a77746a56f292a6dccac91f4
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-02 17:12:38 -0400

    [Shoot] Added dead zone.

--- Assets/Scripts/MicroGames/Shoot/ShootMicroGameController.cs
+++ Assets/Scripts/MicroGames/Shoot/ShootMicroGameController.cs
@@ -3,9 +3,11 @@ namespace Auboreal {
 	public class ShootMicroGameController : AMicroGameController {
 
 		public ShootSpaceShip shootSpaceShip;
+		public ShootDeadZone shootDeadZone;
 		
 		protected override void OnGameStarted() {
 			base.OnGameStarted();
+			shootDeadZone.OnGameStarted(this);
 			shootSpaceShip.OnGameStarted(this);
 		}
 
