dfa60a16117649c18e80becd39a0ceb98afc627a
NnYGames <NnYMakingGames@gmail.com>
2023-10-03 00:29:36 +0300

    Implemented delete and explosion of spaceship

--- Assets/Scripts/MicroGames/FuelUp/FuelUpSpaceship.cs
+++ Assets/Scripts/MicroGames/FuelUp/FuelUpSpaceship.cs
@@ -8,20 +8,11 @@ namespace Auboreal
     {
         [SerializeField] private Transform spacestation;
         [SerializeField] private float speed = 10f;
+        [SerializeField] private GameObject explosion;
 
         public FuelUpMicroGameController controller;
 
         private bool move = true;
-        // Start is called before the first frame update
-        void Start()
-        {
-            /*Vector3 look = transform.InverseTransformPoint(spacestation.position);
-            float angle = Mathf.Atan2(look.y, look.x) * Mathf.Rad2Deg;
-
-            transform.Rotate(0, 0, angle);*/
-            //transform.LookAt(spacestation);
-        }
-
         // Update is called once per frame
         void Update()
         {
@@ -33,17 +24,18 @@ namespace Auboreal
         {
             if (collision.gameObject.name == "FuelLocation")
             {
-                Debug.Log("Success");
                 collision.transform.GetComponentInParent<SpaceStation>().rotate = false;
                 move = false;
 
             }
             if (collision.gameObject.name == "Spacestation")
             {
-                Debug.Log("Fail");
                 collision.transform.GetComponent<SpaceStation>().rotate = false;
                 move = false;
                 controller.lost = true;
+                GameObject summonedExplosion = Instantiate(explosion, transform.position, collision.transform.rotation);
+                summonedExplosion.transform.SetParent(collision.transform);
+                Destroy(gameObject);
             }
         }
     }
