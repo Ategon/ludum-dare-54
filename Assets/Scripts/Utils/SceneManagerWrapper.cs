5c743cb9fcb68b9c5ea410cfac7c94bfa34cbc32
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-01 23:14:36 -0400

    Fixed intermediate logic.

--- Assets/Scripts/Utils/SceneManagerWrapper.cs
+++ Assets/Scripts/Utils/SceneManagerWrapper.cs
@@ -1,82 +1,86 @@
 ﻿namespace Auboreal {
 
-	using UnityEngine;
-	using UnityEngine.SceneManagement;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-	public class SceneManagerWrapper {
+    public class SceneManagerWrapper {
 
-		private const string MainMenuScene = "MainMenu";
-		private const string IntermediateScene = "_Intermediate";
+        private const string MainMenuScene = "MainMenu";
+        private const string IntermediateScene = "_Intermediate";
 
-		private PersistentData.MicroGame m_CurrentMicroGame;
+        private bool m_IsSwitching = false;
+        private PersistentData.MicroGame m_CurrentMicroGame;
 
-		public void SwitchScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode,
-			bool isComingFromMenu) {
-			if (microGame == null) {
-				Debug.LogError("Micro Game passed to switch scene is null");
-				return;
-			}
+        public void SwitchScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode,
+            bool isComingFromMenu) {
+            if (m_IsSwitching) {
+                Debug.LogWarning("Scene switch already in progress. Ignoring request.");
+                return;
+            }
 
-			if (isComingFromMenu && !IsSceneLoaded(IntermediateScene)) {
-				var loadInter = SceneManager.LoadSceneAsync(IntermediateScene, loadSceneMode);
+            if (microGame == null) {
+                Debug.LogError("Micro Game passed to switch scene is null");
+                return;
+            }
 
-				loadInter.completed += (_) => {
-					if (IsSceneLoaded(MainMenuScene)) {
-						var unloadOp = SceneManager.UnloadSceneAsync(MainMenuScene);
+            m_IsSwitching = true;
 
-						if (unloadOp != null) {
-							unloadOp.completed += (_) => LoadIntermediateScene(microGame, loadSceneMode);
-						}
-					}
-					else {
-						LoadIntermediateScene(microGame, loadSceneMode);
-					}
-				};
-			}
-			else {
-				var unloadOp = SceneManager.UnloadSceneAsync(m_CurrentMicroGame.sceneName);
+            LoadIntermediateScene(microGame, loadSceneMode, isComingFromMenu);
+        }
 
-				if (unloadOp != null) {
-					unloadOp.completed += (_) => LoadIntermediateScene(microGame, loadSceneMode);
-				}
-			}
-		}
+        private void LoadIntermediateScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode, bool isComingFromMenu) {
+            if (!IsSceneLoaded(IntermediateScene)) {
+                var loadInter = SceneManager.LoadSceneAsync(IntermediateScene, loadSceneMode);
+                loadInter.completed += (op) => {
+                    // After the intermediate scene is loaded, unload the appropriate scene (MainMenu or current MicroGame).
+                    UnloadPreviousScene(isComingFromMenu, () => {
+                        LoadNewScene(microGame, loadSceneMode);
+                    });
+                };
+            } else {
+                UnloadPreviousScene(isComingFromMenu, () => {
+                    LoadNewScene(microGame, loadSceneMode);
+                });
+            }
+        }
 
-		private void LoadIntermediateScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode) {
-			var loadInter = SceneManager.LoadSceneAsync(IntermediateScene, loadSceneMode);
+        private void UnloadPreviousScene(bool isComingFromMenu, System.Action onComplete) {
+            string sceneToUnload = isComingFromMenu ? MainMenuScene : m_CurrentMicroGame.sceneName;
 
-			loadInter.completed += (op) => {
-				var unloadOp = SceneManager.UnloadSceneAsync(IntermediateScene);
+            if (IsSceneLoaded(sceneToUnload)) {
+                var unloadOp = SceneManager.UnloadSceneAsync(sceneToUnload);
+                unloadOp.completed += (_) => onComplete();
+            } else {
+                onComplete();
+            }
+        }
 
-				if (unloadOp != null) {
-					unloadOp.completed += (_) => LoadNewScene(microGame, loadSceneMode);
-				}
-			};
-		}
+        private void LoadNewScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode) {
+            m_CurrentMicroGame = microGame;
+            SceneManager.sceneLoaded += OnMicroGameSceneLoaded;
+            SceneManager.LoadSceneAsync(microGame.sceneName, loadSceneMode);
+        }
 
-		private void LoadNewScene(PersistentData.MicroGame microGame, LoadSceneMode loadSceneMode) {
-			m_CurrentMicroGame = microGame;
-			SceneManager.sceneLoaded += OnMicroGameSceneLoaded;
-			SceneManager.LoadSceneAsync(microGame.sceneName, loadSceneMode);
-		}
+        private void OnMicroGameSceneLoaded(Scene microGameScene, LoadSceneMode sceneMode) {
+            SceneManager.sceneLoaded -= OnMicroGameSceneLoaded;
 
-		private void OnMicroGameSceneLoaded(Scene microGameScene, LoadSceneMode sceneMode) {
-			SceneManager.sceneLoaded -= OnMicroGameSceneLoaded;
+            // Unload intermediate scene after the new scene has been loaded
+            if (IsSceneLoaded(IntermediateScene)) {
+                SceneManager.UnloadSceneAsync(IntermediateScene);
+            }
 
-			var controller = Object.FindObjectOfType<AMicroGameController>();
+            var controller = Object.FindObjectOfType<AMicroGameController>();
+            if (controller != null) {
+                controller.Initialize(m_CurrentMicroGame);
+            } else {
+                Debug.LogError($"No MicroGameController found in scene: {microGameScene.name}");
+            }
 
-			if (controller != null) {
-				controller.Initialize(m_CurrentMicroGame);
-			}
-			else {
-				Debug.LogError($"No MicroGameController found in scene: {microGameScene.name}");
-			}
-		}
+            m_IsSwitching = false; // Release the lock
+        }
 
-		private bool IsSceneLoaded(string sceneName) {
-			return SceneManager.GetSceneByName(sceneName).isLoaded;
-		}
-
-	}
-
+        private bool IsSceneLoaded(string sceneName) {
+            return SceneManager.GetSceneByName(sceneName).isLoaded;
+        }
+    }
 }
