ad57a6e41be49f6bcb35cc6feaad1b6e2a01fac5
Ategon <benjamin@barbeau.net>
2023-10-01 20:37:16 -0400

    Cahnge menu buttons

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -12,7 +12,17 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        //Quit the application
+        Application.Quit();
+#endif
 
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     public void Leaderboard()
