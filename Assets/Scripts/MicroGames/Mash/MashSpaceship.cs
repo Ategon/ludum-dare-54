6bb1a835956cce27fe170bc19440576dfabb0af3
Ayoub Gharbi <contact@ayoub-gharbi.org>
2023-10-04 08:43:23 -0400

    [Mash] added onfailure and onsuccess logic.

--- Assets/Scripts/MicroGames/Mash/MashSpaceship.cs
+++ Assets/Scripts/MicroGames/Mash/MashSpaceship.cs
@@ -1,34 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using Auboreal;
-using UnityEngine;
+namespace Auboreal {
 
-public class MashSpaceship : MonoBehaviour
-{
-    [SerializeField] private GameObject explosion;
+	using UnityEngine;
 
-    [SerializeField] private Transform sun;
-    [SerializeField] private float pullSpeed;
-    [SerializeField] private float spaceshipSpeed;
+	public class MashSpaceship : AMicroGameEntity {
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, sun.position, pullSpeed * Time.deltaTime);
-    }
+		[SerializeField]
+		private GameObject explosion;
 
-    public void Move()
-    {
-        float factor = 1 + (1 - Time.deltaTime) / 3;
-        transform.position = new Vector2(transform.position.x + spaceshipSpeed * factor, transform.position.y);
-        transform.rotation = new Quaternion(0, 0, transform.rotation.z + Random.Range(-1f, 1f)/60, transform.rotation.w);
-    }
+		[SerializeField]
+		private Transform sun;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        FindObjectOfType<MashMicroGameController>().lost = true;
-        var summonedExplosion = Instantiate(explosion, this.transform.position, this.transform.rotation);
-        summonedExplosion.transform.SetParent(this.transform.parent);
-        Destroy(this.gameObject);
-    }
+		[SerializeField]
+		private float pullSpeed;
+
+		[SerializeField]
+		private float spaceshipSpeed;
+
+		public override void OnStart(AMicroGameController mGameController) {
+			base.OnStart(mGameController);
+		}
+
+		private void Update() {
+			this.transform.position = Vector2.MoveTowards(transform.position, sun.position, pullSpeed * Time.deltaTime);
+		}
+
+		public void Move() {
+			var factor = 1 + (1 - Time.deltaTime) / 3;
+
+			this.transform.position =
+				new Vector2(transform.position.x + spaceshipSpeed * factor, this.transform.position.y);
+
+			this.transform.rotation = new Quaternion(0, 0, transform.rotation.z + Random.Range(-1f, 1f) / 60,
+				this.transform.rotation.w);
+		}
+
+		private void OnTriggerEnter2D(Collider2D collision) {
+			var summonedExplosion = Instantiate(explosion, this.transform.position, this.transform.rotation);
+			summonedExplosion.transform.SetParent(this.transform.parent);
+			Destroy(this.gameObject);
+			this.microGameController.OnFailure();
+		}
+
+		public override void OnEnd() { }
+
+	}
+
 }
